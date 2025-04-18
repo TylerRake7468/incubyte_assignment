40686e12b272cb8c66d0241f88939c1ef5bbc3c2
lalitmll <palariya.lalit@mahhindralogistics.com>
2025-04-18 17:46:16 +0530

    test case 6: Support different delimiters in numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
 	def add(numbers)
 		return 0 if numbers.strip.empty?
-		numbers.split(/,|\n/).map(&:to_i).sum
+		delimiter = /,|\n/
+
+		if numbers.start_with?('//')
+			deli_hint, numbers = numbers.split("\n",2)
+			custom_delimiter = deli_hint[2..]
+			delimiter = Regexp.escape(custom_delimiter)
+		end
+		numbers.split(/#{delimiter}/).map(&:to_i).sum
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
 		it "it should handle new lines between numbers " do
 			expect(calc.add("1\n2,3")).to eq(6)
 		end
+
+		it "it should Support different delimiters in numbers " do
+			expect(calc.add("//;\n1;2")).to eq(3)
+		end
 	end
 end
