b1879c52c7d02de0bfe0689f5eb8fc1126d71c1f
lalitmll <palariya.lalit@mahhindralogistics.com>
2025-04-18 17:26:08 +0530

    test case 5: it should handle new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
 	def add(numbers)
 		return 0 if numbers.strip.empty?
-		numbers.split(",").map(&:to_i).sum
+		numbers.split(/,|\n/).map(&:to_i).sum
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,24 +2,26 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
 	describe "#add" do
+		let(:calc){ StringCalculator.new }
+
 		it "return 0 for empty string" do
-			calc = StringCalculator.new
 			expect(calc.add("")).to eq(0) 
 		end
 
 		it "it should return single number for single number" do
-			calc = StringCalculator.new
 			expect(calc.add("1")).to eq(1)
 		end
 
 		it "it should return sum of number for two numbers with comma separated" do
-			calc = StringCalculator.new
 			expect(calc.add("1,5")).to eq(6)
 		end
 
 		it "it should return sum of number for all numbers" do
-			calc = StringCalculator.new
 			expect(calc.add("1,2,3,4,5")).to eq(15)
+		end
+
+		it "it should handle new lines between numbers " do
+			expect(calc.add("1\n2,3")).to eq(6)
 		end
 	end
 end
